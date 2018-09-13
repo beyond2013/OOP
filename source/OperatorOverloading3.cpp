05dbcf320644dd8598a363b0f861c91c06e3d8ac
beyond2013 <imran.shahjee@gmail.com>
2018-09-13 11:52:51 +0500

    Operator Overloading, Nameless Temporary Objects, Postfix Notation

---
+++ source/OperatorOverloading3.cpp
@@ -0,0 +1,34 @@
+// Nameless temporary Objects
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+private:
+unsigned int count;  //count
+public:
+Counter() : count(0) //constructor
+{ }
+Counter(int c) : count(c) //constructor
+{ }
+
+unsigned int get_count() //return count
+{ return count; }
+Counter operator ++ () //increment (prefix)
+{
+++count;
+return Counter(count);
+}
+};
+int main()
+{
+Counter c1, c2;
+cout << "\nc1=" << c1.get_count();
+cout << "\nc2=" << c2.get_count();
+c2= ++c1;  // using overloaded operator ++ in assignment
+cout << "\nc1=" << c1.get_count(); //display again
+cout << "\nc2=" << c2.get_count() << endl;
+return 0;
+}
+
+
