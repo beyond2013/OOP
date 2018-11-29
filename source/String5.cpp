7fe7a949c75914f6d575246b7d2b763107fa08e0
beyond2013 <imran.shahjee@gmail.com>
2018-11-29 11:25:26 +0500

    update String5 for string functions

--- source/String5.cpp
+++ source/String5.cpp
@@ -10,9 +10,37 @@ int main()
 //	str3 = str1 + str2;
 	str3 = str1 + " " + str2;
 	cout <<endl<< str3<<endl;
+	// size()
 	cout << "size of str1: \t" << str1.size()<<endl;
 	cout << "size of str2: \t" << str2.size()<<endl;
 	cout << "size of str3: \t" << str3.size()<<endl;
+	// append()
+	string str4 = "MCS";
+  cout << "\n str4 before append : " << str4;
+	str4.append(" 2nd Semester");
+  cout << "\n str4 after append : " << str4 << endl;
+  //erase()
+	str4.erase(0,3);
+  cout << "\n str4 after erase(0,2) : " << str4 << endl;
+
+	//replace
+	
+  str4.replace(0,5, "ABCS");
+  cout << "\n str4 after replace(0,5,\"ABCS\") : " << str4 << endl;
+
+	// easier example
+  string str5="university of balochistan, Quetta, balochistan";
+  cout << "\n str5 before replace : " << str5 << endl;
+  str5.replace(0,1,"U");
+  cout << "\n str5 after replace(0,1,\"U\") : " << str5 << endl;
+
+  // find example
+	cout << endl<< "b found at location : " << str5.find("b") <<endl;
+
+//  cout << "\n str5 after replace(14,1,\"B\") : " << str5.replace(14,1,"B") << endl;
+
+  cout << "\n str5 after replace(str5.find(\"b\"),1,\"B\") : "
+		<< str5.replace(str5.find("b"),1,"B") << endl;
 	return 0;
 }
 
