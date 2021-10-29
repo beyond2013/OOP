dac68856df044c3a338440ca0e2731159636cca6
beyond2013 <imran.cs.uob@gmail.com>
2021-10-29 11:41:37 +0500

    add Height

---
+++ source/Height.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Height
+{
+	private:
+		int feet;
+		int inches;
+	public:
+		void setheight(int ft, int in)
+		{
+			feet = ft;
+			inches = in;
+		}
+
+		void showheight()
+		{
+			cout << feet << "\'-" << inches <<'\"' << "\n";
+		}
+};
+
+int main ()
+{
+	Height h1, h2;
+	h1.setheight(5,9);
+	h2.setheight(6,4);
+	h1.showheight();
+	h2.showheight();
+
+	return 0;
+}
+
