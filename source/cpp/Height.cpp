27ad80f30a6db9fc7118c077856ac7fab2a287c7
beyond2013 <imran.cs.uob@gmail.com>
2024-05-04 21:04:27 +0500

    add Height program

--- source/cpp/Height.cpp
+++ source/cpp/Height.cpp
@@ -1,31 +1,72 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class Height
 {
-	private:
-		int feet;
-		int inches;
-	public:
-		void setheight(int ft, int in)
-		{
-			feet = ft;
-			inches = in;
-		}
+private:
+	int feet;
+	int inches;
 
-		void showheight()
+public:
+	/* another way of writing
+	Height()
+	{
+		feet = 0;
+		inches = 0;
+	}
+	*/
+
+	Height() : feet(0), inches(0) {} // this is constructor
+
+	void setheight(int ft, int in)
+	{
+		feet = ft;
+		inches = in;
+	}
+
+	void showheight()
+	{
+		cout << feet << "\'-" << inches << '\"' << "\n";
+	}
+
+	void add_height(Height h1, Height h2)
+	{
+		inches = h1.inches + h2.inches;
+		feet = 0;
+		if (inches >= 12)
 		{
-			cout << feet << "\'-" << inches <<'\"' << "\n";
+			inches -= 12;
+			feet++;
 		}
+		feet += h1.feet + h2.feet;
+	}
 };
 
-int main ()
+int main()
 {
-	Height h1, h2, h3;
-	h1.setheight(5,9);
-	h2.setheight(6,4);
+	Height h1, h2, h3; // created 3 objects of class Height, constructor automatically called
+
+	/* taking user input for object
+	 */
+
+	int h1_feet, h1_inches, h2_feet, h2_inches;
+	cout << "\n Enter value for h1.feet ";
+	cin >> h1_feet;
+	cout << "\n Enter value for h1.inches";
+	cin >> h1_inches;
+
+	cout << "\n Enter value for h2.feet ";
+	cin >> h2_feet;
+	cout << "\n Enter value for h2.inches";
+	cin >> h2_inches;
+	h1.setheight(h1_feet, h1_inches);
+	h2.setheight(h2_feet, h2_inches);
+	cout << "h1" << endl;
 	h1.showheight();
+	cout << "h2" << endl;
 	h2.showheight();
-//  h3.showheight();
+
+	h3.add_height(h1, h2);
+	cout << "h3" << endl;
+	444 h3.showheight();
 	return 0;
 }
-
