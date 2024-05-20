8b91b7655228f25f2a093fde188e1972c61d8dbf
beyond2013 <imran.cs.uob@gmail.com>
2024-05-20 21:05:06 +0500

    add copy constructor to Point

--- source/cpp/Point.cpp
+++ source/cpp/Point.cpp
@@ -1,40 +1,56 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-class Point{
-	protected:
+class Point
+{
+protected:
 	int x, y;
 
-	public:
-	Point(): x(0), y(0){}
+public:
+	Point() : x(0), y(0) {} // zero argument constructor
 
-	Point(int xval, int yval): x(xval), y(yval){}
+	// overloading constructors
+	Point(int xval, int yval) : x(xval), y(yval) {} // 2 argument constructor
 
-	void setX(int xval){
+	Point(const Point &temp) : x(temp.x), y(temp.y) {} // copy constructor
+
+	void setX(int xval)
+	{
 		x = xval;
 	}
 
-	void setY(int yval){
-		y=yval;
+	void setY(int yval)
+	{
+		y = yval;
 	}
 
-	int getX(){
+	int getX()
+	{
 		return x;
 	}
 
-	int getY(){
+	int getY()
+	{
 		return y;
+	}
+	void showPoint()
+	{
+		cout << endl;
+		cout << "x = " << x << " y = " << y << endl;
 	}
 };
 
-int main(){
+int main()
+{
 
 	Point p1;
-	Point p2= Point(2,4);
-
-	cout<<"p1.getX() = " << p1.getX();
-	cout<<"p1.getX() = " << p1.getX();
-	cout<<"p2 = " << p2;
+	Point p2 = Point(2, 4);
+	Point p3(p2);
+	cout << "p1.getX() = " << p1.getX();
+	cout << "p1.getY() = " << p1.getY();
+	cout << "p2 = ";
+	p2.showPoint();
+	cout << "p3 = ";
+	p3.showPoint();
 }
-
