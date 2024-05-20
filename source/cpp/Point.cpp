3dc1d68ca2530df385e2e4f567a6600ebcdc2763
beyond2013 <imran.cs.uob@gmail.com>
2024-05-20 21:12:33 +0500

    discuss default vs programmed constructor

--- source/cpp/Point.cpp
+++ source/cpp/Point.cpp
@@ -13,7 +13,7 @@ public:
 	// overloading constructors
 	Point(int xval, int yval) : x(xval), y(yval) {} // 2 argument constructor
 
-	Point(const Point &temp) : x(temp.x), y(temp.y) {} // copy constructor
+	//	Point(const Point &temp) : x(temp.x), y(temp.y) {} // copy constructor
 
 	void setX(int xval)
 	{
@@ -46,7 +46,9 @@ int main()
 
 	Point p1;
 	Point p2 = Point(2, 4);
-	Point p3(p2);
+	// Point p3(p2);
+	Point p3 = p2; // default copy constructor
+
 	cout << "p1.getX() = " << p1.getX();
 	cout << "p1.getY() = " << p1.getY();
 	cout << "p2 = ";
