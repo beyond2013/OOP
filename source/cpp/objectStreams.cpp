2395d7f876a22b4865bc2ae016a04b10d5a820d7
beyond2013 <imran.cs.uob@gmail.com>
2024-06-23 18:36:11 +0500

    add code for object streams

---
+++ source/cpp/objectStreams.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+class Person
+{
+public:
+    std::string name;
+    int age;
+
+    Person() : name(""), age(0) {}                                     // Default constructor
+    Person(const std::string &name, int age) : name(name), age(age) {} // Parameterized constructor
+};
+
+// Overload the insertion (<<) operator
+std::ostream &operator<<(std::ostream &os, const Person &person)
+{
+    os << person.name << " " << person.age;
+    return os;
+}
+
+// Overload the extraction (>>) operator
+std::istream &operator>>(std::istream &is, Person &person)
+{
+    is >> person.name >> person.age;
+    return is;
+}
+
+int main()
+{
+    // Create a Person object
+    Person p1("John Doe", 30);
+
+    // Serialize the object to a file
+    std::ofstream outFile("person.txt");
+    if (outFile.is_open())
+    {
+        outFile << p1;
+        outFile.close();
+    }
+
+    // Deserialize the object from the file
+    Person p2;
+    std::ifstream inFile("person.txt");
+    if (inFile.is_open())
+    {
+        inFile >> p2;
+        inFile.close();
+    }
+
+    // Display the deserialized object
+    std::cout << "Deserialized Person: " << p2 << std::endl;
+
+    return 0;
+}
